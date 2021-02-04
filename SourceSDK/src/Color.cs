7e34556d36be9401b76ac0edd99967b99dcffa76
SupinePandora43 <supinepandoraggg@gmail.com>
2021-02-04 10:07:32 +0500

    Color.h partial dbg.h Solution Project .editorconfig .gitignore

---
+++ SourceSDK/src/Color.cs
@@ -0,0 +1,49 @@
+using System.Runtime.InteropServices;
+
+namespace SourceSDK
+{
+	/// <summary>
+	/// Color struct
+	/// </summary>
+	/// <example>
+	/// Tier0.Dbg.ConColorMsg(new Color(255, 0, 255), "message\n");
+	/// </example>
+	/// <remarks>
+	/// "public/Color.h"
+	/// </remarks>
+	public struct Color
+	{
+		/// <summary>
+		/// Red
+		/// </summary>
+		[MarshalAs(UnmanagedType.U1)]
+		public byte r;
+		/// <summary>
+		/// Green
+		/// </summary>
+		[MarshalAs(UnmanagedType.U1)]
+		public byte g;
+		/// <summary>
+		/// Blue
+		/// </summary>
+		[MarshalAs(UnmanagedType.U1)]
+		public byte b;
+		/// <summary>
+		/// Alpha
+		/// </summary>
+		[MarshalAs(UnmanagedType.U1)]
+		public byte a;
+
+		/// <param name="r">Red</param>
+		/// <param name="g">Green</param>
+		/// <param name="b">Blue</param>
+		/// <param name="a">Alpha</param>
+		public Color(byte r, byte g, byte b, byte a = 255)
+		{
+			this.r = r;
+			this.g = g;
+			this.b = b;
+			this.a = a;
+		}
+	}
+}
---
+++ SourceSDK/src/tier0/dbg.cs
@@ -0,0 +1,119 @@
+using System.Runtime.InteropServices;
+
+namespace SourceSDK.Tier0
+{
+	/// <remarks>
+	/// "public/tier0/dbg.h"
+	/// </remarks>
+	class Dbg
+	{
+		#region
+		[DllImport("tier0", CallingConvention = CallingConvention.Cdecl)]
+		public static extern void Msg([MarshalAs(UnmanagedType.LPUTF8Str)] string msg);
+
+		[DllImport("tier0", CallingConvention = CallingConvention.Cdecl)]
+		public static extern void DMsg([MarshalAs(UnmanagedType.LPUTF8Str)] string groupName, int level, [MarshalAs(UnmanagedType.LPUTF8Str)] string msg);
+
+		[DllImport("tier0", CallingConvention = CallingConvention.Cdecl)]
+		public static extern void MsgV([MarshalAs(UnmanagedType.LPUTF8Str)] string msg, System.ArgIterator arglist);
+		#endregion
+		#region
+		[DllImport("tier0", CallingConvention = CallingConvention.Cdecl)]
+		public static extern void Warning([MarshalAs(UnmanagedType.LPUTF8Str)] string msg);
+
+		[DllImport("tier0", CallingConvention = CallingConvention.Cdecl)]
+		public static extern void DWarning([MarshalAs(UnmanagedType.LPUTF8Str)] string groupName, int level, [MarshalAs(UnmanagedType.LPUTF8Str)] string msg);
+
+		[DllImport("tier0", CallingConvention = CallingConvention.Cdecl)]
+		public static extern void WarningV([MarshalAs(UnmanagedType.LPUTF8Str)] string msg, System.ArgIterator arglist);
+		#endregion
+		#region
+		[DllImport("tier0", CallingConvention = CallingConvention.Cdecl)]
+		public static extern void Log([MarshalAs(UnmanagedType.LPUTF8Str)] string msg);
+
+		[DllImport("tier0", CallingConvention = CallingConvention.Cdecl)]
+		public static extern void DLog([MarshalAs(UnmanagedType.LPUTF8Str)] string groupName, int level, [MarshalAs(UnmanagedType.LPUTF8Str)] string msg);
+
+		[DllImport("tier0", CallingConvention = CallingConvention.Cdecl)]
+		public static extern void LogV([MarshalAs(UnmanagedType.LPUTF8Str)] string msg, System.ArgIterator arglist);
+		#endregion
+		#region
+		[DllImport("tier0", CallingConvention = CallingConvention.Cdecl)]
+		public static extern void Error([MarshalAs(UnmanagedType.LPUTF8Str)] string msg);
+
+		[DllImport("tier0", CallingConvention = CallingConvention.Cdecl)]
+		public static extern void ErrorV([MarshalAs(UnmanagedType.LPUTF8Str)] string msg, System.ArgIterator arglist);
+		#endregion
+		#region
+
+		[DllImport("tier0", CallingConvention = CallingConvention.Cdecl)]
+		public static extern void DevMsg(int level, [MarshalAs(UnmanagedType.LPUTF8Str)] string msg);
+
+		[DllImport("tier0", CallingConvention = CallingConvention.Cdecl)]
+		public static extern void DevWarning(int level, [MarshalAs(UnmanagedType.LPUTF8Str)] string msg);
+
+		[DllImport("tier0", CallingConvention = CallingConvention.Cdecl)]
+		public static extern void DevLog(int level, [MarshalAs(UnmanagedType.LPUTF8Str)] string msg);
+
+
+		[DllImport("tier0", EntryPoint = "?DevMsg@@YAXPEBDZZ", CallingConvention = CallingConvention.Cdecl)]
+		public static extern void DevMsg([MarshalAs(UnmanagedType.LPUTF8Str)] string msg);
+
+		[DllImport("tier0", EntryPoint = "?DevWarning@@YAXPEBDZZ", CallingConvention = CallingConvention.Cdecl)]
+		public static extern void DevWarning([MarshalAs(UnmanagedType.LPUTF8Str)] string msg);
+
+		[DllImport("tier0", EntryPoint = "?DevLog@@YAXPEBDZZ", CallingConvention = CallingConvention.Cdecl)]
+		public static extern void DevLog([MarshalAs(UnmanagedType.LPUTF8Str)] string msg);
+
+
+		[DllImport("tier0", CallingConvention = CallingConvention.Cdecl)]
+		public static extern void ConColorMsg(int level, in Color clr, [MarshalAs(UnmanagedType.LPUTF8Str)] string msg);
+
+		[DllImport("tier0", CallingConvention = CallingConvention.Cdecl)]
+		public static extern void ConMsg(int level, [MarshalAs(UnmanagedType.LPUTF8Str)] string msg);
+
+		[DllImport("tier0", CallingConvention = CallingConvention.Cdecl)]
+		public static extern void ConWarning(int level, [MarshalAs(UnmanagedType.LPUTF8Str)] string msg);
+
+		[DllImport("tier0", CallingConvention = CallingConvention.Cdecl)]
+		public static extern void ConLog(int level, [MarshalAs(UnmanagedType.LPUTF8Str)] string msg);
+
+
+		[DllImport("tier0", EntryPoint = "?ConColorMsg@@YAXAEBVColor@@PEBDZZ", CallingConvention = CallingConvention.Cdecl)]
+		public static extern void ConColorMsg(in Color clr, [MarshalAs(UnmanagedType.LPUTF8Str)] string msg);
+
+		[DllImport("tier0", EntryPoint = "?ConMsg@@YAXPBDZZ", CallingConvention = CallingConvention.Cdecl)]
+		public static extern void ConMsg([MarshalAs(UnmanagedType.LPUTF8Str)] string msg);
+
+		[DllImport("tier0", CallingConvention = CallingConvention.Cdecl)]
+		public static extern void ConWarning([MarshalAs(UnmanagedType.LPUTF8Str)] string msg);
+
+		[DllImport("tier0", CallingConvention = CallingConvention.Cdecl)]
+		public static extern void ConLog([MarshalAs(UnmanagedType.LPUTF8Str)] string msg);
+
+
+		[DllImport("tier0", CallingConvention = CallingConvention.Cdecl)]
+		public static extern void ConDColorMsg(in Color clr, [MarshalAs(UnmanagedType.LPUTF8Str)] string msg);
+
+		[DllImport("tier0", CallingConvention = CallingConvention.Cdecl)]
+		public static extern void ConDMsg([MarshalAs(UnmanagedType.LPUTF8Str)] string msg);
+
+		[DllImport("tier0", CallingConvention = CallingConvention.Cdecl)]
+		public static extern void ConDWarning([MarshalAs(UnmanagedType.LPUTF8Str)] string msg);
+
+		[DllImport("tier0", CallingConvention = CallingConvention.Cdecl)]
+		public static extern void ConDLog([MarshalAs(UnmanagedType.LPUTF8Str)] string msg);
+
+
+		[DllImport("tier0", CallingConvention = CallingConvention.Cdecl)]
+		public static extern void NetMsg(int level, [MarshalAs(UnmanagedType.LPUTF8Str)] string msg);
+
+		[DllImport("tier0", CallingConvention = CallingConvention.Cdecl)]
+		public static extern void NetWarning(int level, [MarshalAs(UnmanagedType.LPUTF8Str)] string msg);
+
+		[DllImport("tier0", CallingConvention = CallingConvention.Cdecl)]
+		public static extern void NetLog(int level, [MarshalAs(UnmanagedType.LPUTF8Str)] string msg);
+
+		#endregion
+	}
+}
