b07dc7cad30d8819cfe854f25ed930e6708248bc
SupinePandora43 <supinepandoraggg@gmail.com>
2021-02-04 13:01:58 +0500

    test ConColorMsg(int, in Color, string)

--- SourceSDK.Test/TestModule.cs
+++ SourceSDK.Test/TestModule.cs
@@ -13,6 +13,7 @@ namespace SourceSDKTest
 		public void Load(ILua lua, bool is_serverside, ModuleAssemblyLoadContext assembly_context)
 		{
 			Dbg.ConColorMsg(new Color(255, 255, 0), "ConColorMsg(in Color, string)\n");
+			Dbg.ConColorMsg(0, new Color(255, 0, 0), "ConColorMsg(int, in Color, string)\n");
 		}
 
 		public void Unload(ILua lua)
