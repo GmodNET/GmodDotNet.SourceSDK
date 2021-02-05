d0a34182b19255d96b0a98ff2c3681773f1b6401
SupinePandora43 <supinepandoraggg@gmail.com>
2021-02-05 22:51:24 +0500

    rename src to public add interfaceh.cs | + CreateInterfaceFn | + Sys_GetFactory()

---
+++ SourceSDK/tier1/interfaceh.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Runtime.InteropServices;
+
+namespace SourceSDK.tier1
+{
+	class interfaceh
+	{
+		public const string CREATEINTERFACE_PROCNAME = "CreateInterface";
+		public delegate IntPtr CreateInterfaceFn(string name, int returnCode);
+
+		public static CreateInterfaceFn Sys_GetFactory(IntPtr module)
+		{
+			IntPtr createInterfaceFnPtr = NativeLibrary.GetExport(module, CREATEINTERFACE_PROCNAME);
+			return Marshal.GetDelegateForFunctionPointer<CreateInterfaceFn>(createInterfaceFnPtr);
+		}
+	}
+}
