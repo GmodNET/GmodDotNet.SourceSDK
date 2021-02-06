75b66d418550540117df5bd5a03384298e24f9ac
SupinePandora43 <supinepandoraggg@gmail.com>
2021-02-06 15:16:48 +0500

    add UnmanagedFunctionPointer

--- SourceSDK/public/filesystemh.cs
+++ SourceSDK/public/filesystemh.cs
@@ -1,16 +1,17 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SourceSDK
 {
-	
 	public struct IFileSystem
 	{
 		public static class Delegates
 		{
+			[UnmanagedFunctionPointer(CallingConvention.Cdecl)]
 			public delegate bool rBool();
 		}
 
