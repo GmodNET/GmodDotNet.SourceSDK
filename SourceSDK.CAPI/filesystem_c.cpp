f62ca5663c5ae606e2e2749e0e12a0932d552c16
SupinePandora43 <supinepandoraggg@gmail.com>
2021-02-10 11:33:45 +0500

    add filesystem.h enums add FILESYSTEM_MAX_SEARCH_PATHS const refactor filesystem_c.cpp add FileSystem.MountSteamContent rename internal class FileSystemH to BaseFileSystem_c

--- SourceSDK.CAPI/filesystem_c.cpp
+++ SourceSDK.CAPI/filesystem_c.cpp
@@ -1,88 +1,70 @@
 #include <filesystem.h>
 
 #pragma region IBaseFileSystem
-
 DLL_EXPORT int IBaseFileSystem_Read(void** ptr, void* pOutput, int size, FileHandle_t file) {
 	IBaseFileSystem* bfs = (IBaseFileSystem*)ptr;
 	return bfs->Read(pOutput, size, file);
 }
-
 DLL_EXPORT int IBaseFileSystem_Write(void** ptr, void const* pInput, int size, FileHandle_t file) {
 	IBaseFileSystem* bfs = (IBaseFileSystem*)ptr;
 	return bfs->Write(pInput, size, file);
 }
-
 DLL_EXPORT FileHandle_t IBaseFileSystem_Open(void** ptr, const char* pFileName, const char* pOptions, const char* pathID = 0) {
 	IBaseFileSystem* bfs = (IBaseFileSystem*)ptr;
 	return bfs->Open(pFileName, pOptions, pathID);
 }
-
 DLL_EXPORT void IBaseFileSystem_Close(void** ptr, FileHandle_t file) {
 	IBaseFileSystem* bfs = (IBaseFileSystem*)ptr;
 	bfs->Close(file);
 }
-
 DLL_EXPORT void IBaseFileSystem_Seek(void** ptr, FileHandle_t file, int pos, FileSystemSeek_t seekType) {
 	IBaseFileSystem* bfs = (IBaseFileSystem*)ptr;
 	bfs->Seek(file, pos, seekType);
 }
-
 DLL_EXPORT unsigned int IBaseFileSystem_Tell(void** ptr, FileHandle_t file) {
 	IBaseFileSystem* bfs = (IBaseFileSystem*)ptr;
 	return bfs->Tell(file);
 }
-
 DLL_EXPORT unsigned int IBaseFileSystem_Size(void** ptr, FileHandle_t file) {
 	IBaseFileSystem* bfs = (IBaseFileSystem*)ptr;
 	return bfs->Size(file);
 }
-
 DLL_EXPORT unsigned int IBaseFileSystem_Size_string_string(void** ptr, const char* pFileName, const char* pPathID = 0) {
 	IBaseFileSystem* bfs = (IBaseFileSystem*)ptr;
 	return bfs->Size(pFileName, pPathID);
 }
-
 DLL_EXPORT void IBaseFileSystem_Flush(void** ptr, FileHandle_t file) {
 	IBaseFileSystem* bfs = (IBaseFileSystem*)ptr;
 	bfs->Flush(file);
 }
-
 DLL_EXPORT bool IBaseFileSystem_Precache(void** ptr, const char* pFileName, const char* pPathID = 0) {
 	IBaseFileSystem* bfs = (IBaseFileSystem*)ptr;
 	return bfs->Precache(pFileName, pPathID);
 }
-
 DLL_EXPORT bool IBaseFileSystem_FileExists(void** ptr, const char* pFileName, const char* pPathID = 0) {
 	IBaseFileSystem* bfs = (IBaseFileSystem*)ptr;
 	return bfs->FileExists(pFileName, pPathID);
 }
-
 DLL_EXPORT bool IBaseFileSystem_IsFileWritable(void** ptr, const char* pFileName, const char* pPathID = 0) {
 	IBaseFileSystem* bfs = (IBaseFileSystem*)ptr;
 	return bfs->IsFileWritable(pFileName, pPathID);
 }
-
 DLL_EXPORT bool IBaseFileSystem_SetFileWritable(void** ptr, char const* pFileName, bool writable, const char* pPathID = 0) {
 	IBaseFileSystem* bfs = (IBaseFileSystem*)ptr;
 	return bfs->SetFileWritable(pFileName, writable, pPathID);
 }
-
 DLL_EXPORT long IBaseFileSystem_GetFileTime(void** ptr, const char* pFileName, const char* pPathID = 0) {
 	IBaseFileSystem* bfs = (IBaseFileSystem*)ptr;
 	return bfs->GetFileTime(pFileName, pPathID);
 }
-
-
 DLL_EXPORT bool IBaseFileSystem_ReadFile(void** ptr, const char* pFileName, const char* pPath, CUtlBuffer& buf, int nMaxBytes = 0, int nStartingByte = 0, FSAllocFunc_t pfnAlloc = NULL) {
 	IBaseFileSystem* bfs = (IBaseFileSystem*)ptr;
 	return bfs->ReadFile(pFileName, pPath, buf, nMaxBytes, nStartingByte, pfnAlloc);
 }
-
 DLL_EXPORT bool IBaseFileSystem_WriteFile(void** ptr, const char* pFileName, const char* pPath, CUtlBuffer& buf) {
 	IBaseFileSystem* bfs = (IBaseFileSystem*)ptr;
 	return bfs->WriteFile(pFileName, pPath, buf);
 }
-
 DLL_EXPORT bool IBaseFileSystem_UnzipFile(void** ptr, const char* pFileName, const char* pPath, const char* pDestination) {
 	IBaseFileSystem* bfs = (IBaseFileSystem*)ptr;
 	return bfs->UnzipFile(pFileName, pPath, pDestination);
@@ -92,6 +74,11 @@ DLL_EXPORT bool IBaseFileSystem_UnzipFile(void** ptr, const char* pFileName, con
 DLL_EXPORT bool IFileSystem_IsSteam(void** fsPtr) {
 	IFileSystem* fs = (IFileSystem*)fsPtr;
 	return fs->IsSteam();
+}
+
+DLL_EXPORT FilesystemMountRetval_t IFileSystem_MountSteamContent(void** fsPtr, int nExtraAppId = -1) {
+	IFileSystem* fs = (IFileSystem*)fsPtr;
+	return fs->MountSteamContent(nExtraAppId);
 }
 
 DLL_EXPORT void IFileSystem_PrintSearchPaths(void** fsPtr) {
--- SourceSDK/public/filesystemh.cs
+++ SourceSDK/public/filesystemh.cs
@@ -4,14 +4,183 @@ using System.Runtime.InteropServices;
 
 namespace GmodNET.SourceSDK
 {
+	#region Enums
+
+	public enum EPureServerFileClass
+	{
+		/// <summary>
+		/// dummy debugging value
+		/// </summary>
+		ePureServerFileClass_Unknown = -1,
+		ePureServerFileClass_Any = 0,
+		ePureServerFileClass_AnyTrusted = 1,
+		ePureServerFileClass_CheckHash = 2,
+	}
 	public enum FileSystemSeek_t
 	{
 		FILESYSTEM_SEEK_HEAD = 0,
 		FILESYSTEM_SEEK_CURRENT = 1,
 		FILESYSTEM_SEEK_TAIL = 2,
-	};
+	}
+	public enum FileSystem_Handle
+	{
+		FILESYSTEM_INVALID_FIND_HANDLE = -1
+	}
+	public enum FileWarningLevel_t
+	{
+		/// <summary>A problem!</summary>
+		FILESYSTEM_WARNING = -1,
+		/// <summary>Don't print anything</summary>
+		FILESYSTEM_WARNING_QUIET = 0,
+		///<summary>On shutdown, report names of files left unclosed</summary>
+		FILESYSTEM_WARNING_REPORTUNCLOSED = 1,
+		/// <summary>Report number of times a file was opened, closed</summary>
+		FILESYSTEM_WARNING_REPORTUSAGE = 2,
+		/// <summary>Report all open/close events to console ( !slow! )</summary>
+		FILESYSTEM_WARNING_REPORTALLACCESSES = 3,
+		/// <summary>Report all open/close/read events to the console ( !slower! )</summary>
+		FILESYSTEM_WARNING_REPORTALLACCESSES_READ = 4,
+		/// <summary>Report all open/close/read/write events to the console ( !slower! )</summary>
+		FILESYSTEM_WARNING_REPORTALLACCESSES_READWRITE = 5,
+		/// <summary>Report all open/close/read/write events and all async I/O file events to the console ( !slower(est)! )</summary>
+		FILESYSTEM_WARNING_REPORTALLACCESSES_ASYNC = 6,
+	}
+	/// <summary>
+	/// search path filtering
+	/// </summary>
+	public enum PathTypeFilter_t
+	{
+		/// <summary>
+		/// no filtering, all search path types match
+		/// </summary>
+		FILTER_NONE = 0,
+		/// <summary>
+		/// pack based search paths are culled (maps and zips)
+		/// </summary>
+		FILTER_CULLPACK = 1,
+		/// <summary>
+		/// non-pack based search paths are culled
+		/// </summary>
+		FILTER_CULLNONPACK = 2,
+	}
+	// search path querying (bit flags)
+	public enum PATH
+	{
+		/// <summary>
+		/// normal path, not pack based
+		/// </summary>
+		PATH_IS_NORMAL = 0x00,
+		/// <summary>
+		/// path is a pack file
+		/// </summary>
+		PATH_IS_PACKFILE = 0x01,
+		/// <summary>
+		/// path is a map pack file
+		/// </summary>
+		PATH_IS_MAPPACKFILE = 0x02,
+		/// <summary>
+		/// path is the remote filesystem
+		/// </summary>
+		PATH_IS_REMOTE = 0x04,
+	}
+	public enum DVDMode_t
+	{
+		/// <summary>
+		/// not using dvd
+		/// </summary>
+		DVDMODE_OFF = 0,
+		/// <summary>
+		/// dvd device only
+		/// </summary>
+		DVDMODE_STRICT = 1,
+		/// <summary>
+		/// dev mode, mutiple devices ok
+		/// </summary>
+		DVDMODE_DEV = 2
+	}
+	public enum FilesystemMountRetval_t
+	{
+		FILESYSTEM_MOUNT_OK = 0,
+		FILESYSTEM_MOUNT_FAILED = 1
+	}
+	public enum SearchPathAdd_t
+	{
+		/// <summary>
+		/// First path searched
+		/// </summary>
+		PATH_ADD_TO_HEAD = 0,
+		/// <summary>
+		/// Last path searched
+		/// </summary>
+		PATH_ADD_TO_TAIL = 1
+	}
+	public enum FilesystemOpenExFlags_t
+	{
+		FSOPEN_UNBUFFERED = (1 << 0),
+		FSOPEN_FORCE_TRACK_CRC = (1 << 1),      // This makes it calculate a CRC for the file (if the file came from disk) regardless 
+												// of the IFileList passed to RegisterFileWhitelist.
+		FSOPEN_NEVERINPACK = (1 << 2),      // 360 only, hint to FS that file is not allowed to be in pack file
+	}
+	public enum FSAsyncStatus_t
+	{
+		FSASYNC_ERR_NOT_MINE = -8,  // Filename not part of the specified file system, try a different one.  (Used internally to find the right filesystem)
+		FSASYNC_ERR_RETRY_LATER = -7,   // Failure for a reason that might be temporary.  You might retry, but not immediately.  (E.g. Network problems)
+		FSASYNC_ERR_ALIGNMENT = -6, // read parameters invalid for unbuffered IO
+		FSASYNC_ERR_FAILURE = -5,   // hard subsystem failure
+		FSASYNC_ERR_READING = -4,   // read error on file
+		FSASYNC_ERR_NOMEMORY = -3,  // out of memory for file read
+		FSASYNC_ERR_UNKNOWNID = -2, // caller's provided id is not recognized
+		FSASYNC_ERR_FILEOPEN = -1,  // filename could not be opened (bad path, not exist, etc)
+		FSASYNC_OK = 0, // operation is successful
+		FSASYNC_STATUS_PENDING = 1,         // file is properly queued, waiting for service
+		FSASYNC_STATUS_INPROGRESS = 2,      // file is being accessed
+		FSASYNC_STATUS_ABORTED = 3,         // file was aborted by caller
+		FSASYNC_STATUS_UNSERVICED = 4,      // file is not yet queued
+	}
+	public enum FSAsyncFlags_t
+	{
+		/// <summary>
+		/// do the allocation for dataPtr, but don't free
+		/// </summary>
+		FSASYNC_FLAGS_ALLOCNOFREE = (1 << 0),
+		/// <summary>
+		/// free the memory for the dataPtr post callback
+		/// </summary>
+		FSASYNC_FLAGS_FREEDATAPTR = (1 << 1),
+		/// <summary>
+		/// Actually perform the operation synchronously. Used to simplify client code paths
+		/// </summary>
+		FSASYNC_FLAGS_SYNC = (1 << 2),
+		/// <summary>
+		/// allocate an extra byte and null terminate the buffer read in
+		/// </summary>
+		FSASYNC_FLAGS_NULLTERMINATE = (1 << 3)
+	}
+	/// <summary>
+	/// Return value for CheckFileCRC.
+	/// </summary>
+	public enum EFileCRCStatus
+	{
+		/// <summary>
+		/// We don't have this file. 
+		/// </summary>
+		k_eFileCRCStatus_CantOpenFile = 0,
+		k_eFileCRCStatus_GotCRC = 1,
+		k_eFileCRCStatus_FileInVPK = 2
+	}
+	/// <summary>
+	/// Used in CacheFileCRCs.
+	/// </summary>
+	public enum ECacheCRCType
+	{
+		k_eCacheCRCType_SingleFile = 0,
+		k_eCacheCRCType_Directory = 1,
+		k_eCacheCRCType_Directory_Recursive = 2
+	}
 
-	internal static class FileSystemH
+	#endregion
+
+	internal static class BaseFileSystem_c
 	{
 		[DllImport("sourcesdkc")]
 		internal static extern int IBaseFileSystem_Read(IntPtr ptr, IntPtr output, int size, IntPtr file);
@@ -49,6 +218,13 @@ namespace GmodNET.SourceSDK
 
 		//TODO: CUtlBuffer
 	}
+
+	internal static class FileSystem_c
+	{
+		[DllImport("sourcesdkc")]
+		internal static extern FilesystemMountRetval_t IFileSystem_MountSteamContent(IntPtr ptr, int extraAppId = -1);
+	}
+
 	internal interface IBaseFileSystem
 	{
 		int Read(IntPtr output, int size, IntPtr file);
@@ -77,7 +253,7 @@ namespace GmodNET.SourceSDK
 	{
 		bool IsSteam();
 
-
+		FilesystemMountRetval_t MountSteamContent(int extraAppId = -1);
 
 		void PrintSearchPaths();
 	}
@@ -92,29 +268,31 @@ namespace GmodNET.SourceSDK
 
 		public BaseFileSystem(IntPtr ptr) => this.ptr = ptr;
 
-		public int Read(IntPtr output, int size, IntPtr file) => FileSystemH.IBaseFileSystem_Read(ptr, output, size, file);
-		public int Write(IntPtr input, int size, IntPtr file) => FileSystemH.IBaseFileSystem_Write(ptr, input, size, file);
+		public int Read(IntPtr output, int size, IntPtr file) => BaseFileSystem_c.IBaseFileSystem_Read(ptr, output, size, file);
+		public int Write(IntPtr input, int size, IntPtr file) => BaseFileSystem_c.IBaseFileSystem_Write(ptr, input, size, file);
 
-		public IntPtr Open(string fileName, string options, string pathID) => FileSystemH.IBaseFileSystem_Open(ptr, fileName, options, pathID);
-		public void Close(IntPtr file) => FileSystemH.IBaseFileSystem_Close(ptr, file);
+		public IntPtr Open(string fileName, string options, string pathID) => BaseFileSystem_c.IBaseFileSystem_Open(ptr, fileName, options, pathID);
+		public void Close(IntPtr file) => BaseFileSystem_c.IBaseFileSystem_Close(ptr, file);
 
-		public void Seek(IntPtr file, int pos, in FileSystemSeek_t seekType) => FileSystemH.IBaseFileSystem_Seek(ptr, file, pos, seekType);
-		public uint Tell(IntPtr file) => FileSystemH.IBaseFileSystem_Tell(ptr, file);
-		public uint Size(IntPtr file) => FileSystemH.IBaseFileSystem_Size(ptr, file);
-		public uint Size(string fileName, string pathID) => FileSystemH.IBaseFileSystem_Size_string_string(ptr, fileName, pathID);
+		public void Seek(IntPtr file, int pos, in FileSystemSeek_t seekType) => BaseFileSystem_c.IBaseFileSystem_Seek(ptr, file, pos, seekType);
+		public uint Tell(IntPtr file) => BaseFileSystem_c.IBaseFileSystem_Tell(ptr, file);
+		public uint Size(IntPtr file) => BaseFileSystem_c.IBaseFileSystem_Size(ptr, file);
+		public uint Size(string fileName, string pathID) => BaseFileSystem_c.IBaseFileSystem_Size_string_string(ptr, fileName, pathID);
 
-		public void Flush(IntPtr file) => FileSystemH.IBaseFileSystem_Flush(ptr, file);
-		public bool Precache(string fileName, string pathID) => FileSystemH.IBaseFileSystem_Precache(ptr, fileName, pathID);
+		public void Flush(IntPtr file) => BaseFileSystem_c.IBaseFileSystem_Flush(ptr, file);
+		public bool Precache(string fileName, string pathID) => BaseFileSystem_c.IBaseFileSystem_Precache(ptr, fileName, pathID);
 
-		public bool FileExists(string fileName, string pathID) => FileSystemH.IBaseFileSystem_FileExists(ptr, fileName, pathID);
-		public bool IsFileWriteable(string fileName, string pathID) => FileSystemH.IBaseFileSystem_IsFileWritable(ptr, fileName, pathID);
-		public bool SetFileWriteable(string fileName, bool writeable, string pathID) => FileSystemH.IBaseFileSystem_SetFileWritable(ptr, fileName, writeable, pathID);
+		public bool FileExists(string fileName, string pathID) => BaseFileSystem_c.IBaseFileSystem_FileExists(ptr, fileName, pathID);
+		public bool IsFileWriteable(string fileName, string pathID) => BaseFileSystem_c.IBaseFileSystem_IsFileWritable(ptr, fileName, pathID);
+		public bool SetFileWriteable(string fileName, bool writeable, string pathID) => BaseFileSystem_c.IBaseFileSystem_SetFileWritable(ptr, fileName, writeable, pathID);
 
-		public long GetFileTime(string fileName, string pathID) => FileSystemH.IBaseFileSystem_GetFileTime(ptr, fileName, pathID);
+		public long GetFileTime(string fileName, string pathID) => BaseFileSystem_c.IBaseFileSystem_GetFileTime(ptr, fileName, pathID);
 	}
 
 	public class FileSystem : IAppSystem, IBaseFileSystem, IFileSystem
 	{
+		public const int FILESYSTEM_MAX_SEARCH_PATHS = 128;
+
 		public const string FILESYSTEM_INTERFACE_VERSION = "VFileSystem022";
 
 		public FileSystem(IntPtr ptr) : base(ptr) { }
@@ -126,6 +304,11 @@ namespace GmodNET.SourceSDK
 			return IFileSystem_IsSteam(ptr);
 		}
 
+		public FilesystemMountRetval_t MountSteamContent(int extraAppId = -1)
+		{
+			return FileSystem_c.IFileSystem_MountSteamContent(ptr, extraAppId);
+		}
+
 		[DllImport("sourcesdkc")]
 		internal static extern void IFileSystem_PrintSearchPaths(IntPtr ptr);
 		public void PrintSearchPaths()
@@ -133,24 +316,24 @@ namespace GmodNET.SourceSDK
 			IFileSystem_PrintSearchPaths(ptr);
 		}
 
-		public int Read(IntPtr output, int size, IntPtr file) => FileSystemH.IBaseFileSystem_Read(ptr, output, size, file);
-		public int Write(IntPtr input, int size, IntPtr file) => FileSystemH.IBaseFileSystem_Write(ptr, input, size, file);
+		public int Read(IntPtr output, int size, IntPtr file) => BaseFileSystem_c.IBaseFileSystem_Read(ptr, output, size, file);
+		public int Write(IntPtr input, int size, IntPtr file) => BaseFileSystem_c.IBaseFileSystem_Write(ptr, input, size, file);
 
-		public IntPtr Open(string fileName, string options, string pathID) => FileSystemH.IBaseFileSystem_Open(ptr, fileName, options, pathID);
-		public void Close(IntPtr file) => FileSystemH.IBaseFileSystem_Close(ptr, file);
+		public IntPtr Open(string fileName, string options, string pathID) => BaseFileSystem_c.IBaseFileSystem_Open(ptr, fileName, options, pathID);
+		public void Close(IntPtr file) => BaseFileSystem_c.IBaseFileSystem_Close(ptr, file);
 
-		public void Seek(IntPtr file, int pos, in FileSystemSeek_t seekType) => FileSystemH.IBaseFileSystem_Seek(ptr, file, pos, seekType);
-		public uint Tell(IntPtr file) => FileSystemH.IBaseFileSystem_Tell(ptr, file);
-		public uint Size(IntPtr file) => FileSystemH.IBaseFileSystem_Size(ptr, file);
-		public uint Size(string fileName, string pathID) => FileSystemH.IBaseFileSystem_Size_string_string(ptr, fileName, pathID);
+		public void Seek(IntPtr file, int pos, in FileSystemSeek_t seekType) => BaseFileSystem_c.IBaseFileSystem_Seek(ptr, file, pos, seekType);
+		public uint Tell(IntPtr file) => BaseFileSystem_c.IBaseFileSystem_Tell(ptr, file);
+		public uint Size(IntPtr file) => BaseFileSystem_c.IBaseFileSystem_Size(ptr, file);
+		public uint Size(string fileName, string pathID) => BaseFileSystem_c.IBaseFileSystem_Size_string_string(ptr, fileName, pathID);
 
-		public void Flush(IntPtr file) => FileSystemH.IBaseFileSystem_Flush(ptr, file);
-		public bool Precache(string fileName, string pathID) => FileSystemH.IBaseFileSystem_Precache(ptr, fileName, pathID);
+		public void Flush(IntPtr file) => BaseFileSystem_c.IBaseFileSystem_Flush(ptr, file);
+		public bool Precache(string fileName, string pathID) => BaseFileSystem_c.IBaseFileSystem_Precache(ptr, fileName, pathID);
 
-		public bool FileExists(string fileName, string pathID) => FileSystemH.IBaseFileSystem_FileExists(ptr, fileName, pathID);
-		public bool IsFileWriteable(string fileName, string pathID) => FileSystemH.IBaseFileSystem_IsFileWritable(ptr, fileName, pathID);
-		public bool SetFileWriteable(string fileName, bool writeable, string pathID) => FileSystemH.IBaseFileSystem_SetFileWritable(ptr, fileName, writeable, pathID);
+		public bool FileExists(string fileName, string pathID) => BaseFileSystem_c.IBaseFileSystem_FileExists(ptr, fileName, pathID);
+		public bool IsFileWriteable(string fileName, string pathID) => BaseFileSystem_c.IBaseFileSystem_IsFileWritable(ptr, fileName, pathID);
+		public bool SetFileWriteable(string fileName, bool writeable, string pathID) => BaseFileSystem_c.IBaseFileSystem_SetFileWritable(ptr, fileName, writeable, pathID);
 
-		public long GetFileTime(string fileName, string pathID) => FileSystemH.IBaseFileSystem_GetFileTime(ptr, fileName, pathID);
+		public long GetFileTime(string fileName, string pathID) => BaseFileSystem_c.IBaseFileSystem_GetFileTime(ptr, fileName, pathID);
 	}
 }
