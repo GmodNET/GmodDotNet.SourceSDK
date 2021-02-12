364782bb69a5418af320b169d29346c57ac06941
SupinePandora43 <supinepandoraggg@gmail.com>
2021-02-12 12:09:10 +0500

    add prints xD

--- .github/workflows/test.lua
+++ .github/workflows/test.lua
@@ -2,11 +2,13 @@ hook.Add("Tick", "CloseServer", engine.CloseServer)
 require("dotnet")
 
 local function run_test()
+	print("loading")
 	local module_loaded = dotnet.load("SourceSDKTest")
 	assert(module_loaded)
 
 	-- uhm
 
+	print("unloading")
 	local module_unloaded = dotnet.unload("SourceSDKTest")
 	assert(module_unloaded)
 end
