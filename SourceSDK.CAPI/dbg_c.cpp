993f1221eadb26f57e7ca4e2385665253dea8016
SupinePandora43 <supinepandoraggg@gmail.com>
2021-02-21 21:47:52 +0500

    inline DBG_CDbgFmtMsg implementation

--- SourceSDK.CAPI/dbg_c.cpp
+++ SourceSDK.CAPI/dbg_c.cpp
@@ -2,5 +2,8 @@
 #include<tier0/dbg.h>
 
 DLL_EXPORT const char* DBG_CDbgFmtMsg(const char* format, va_list args) {
-	return CDbgFmtMsg(format, args);
+	char chars[256];
+	Tier0Internal_vsntprintf(chars, sizeof(chars) - 1, format, args);
+	chars[sizeof(chars) - 1] = 0;
+	return chars;
 }
